91ad0de75d7d0483353874c069dcd8b325b3e8a5
Adam Koziak <adam.koziak2@gmail.com>
2025-03-30 15:05:55 -0400

    rendering instructions

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # MARL_boids
+
+## Installation
+For WSL:
+```
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Render the Environment Interactively
+```
+python render_test.py
+```
+Control two agents with a,d or left/right arrow keys
--- render_test.py
+++ render_test.py
@@ -1,11 +1,8 @@
 import torch
-from vmas.simulator.utils import ScenarioUtils
-from vmas.simulator.core import World
 from torch import Tensor
 from vmas import make_env, render_interactively
 
 from MyScenario import MyScenario
-#from example_scenario import MyScenario
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -18,6 +15,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 render_interactively(
     MyScenario(),
+    #"discovery",
     control_two_agents=True,
     save_render=False,
     display_info=True,

798a854451f08618c9b20ee7c5475bfff6a28bf2
garycodes <65249062+garycodes@users.noreply.github.com>
2025-04-01 14:11:39 -0400

    Added goal taking

--- MyScenario.py
+++ MyScenario.py
@@ -1,11 +1,4 @@
 from vmas.simulator.core import Agent, Box, Landmark, Sphere, World
-# !pip install vmas
-# !apt-get update
-# !apt-get install -y x11-utils python3-opengl xvfb
-# !pip install pyvirtualdisplay
-# import pyvirtualdisplay
-# display = pyvirtualdisplay.Display(visible=False, size=(1400, 900))
-# display.start()
 from vmas.simulator.scenario import BaseScenario
 import torch
 from torch import Tensor
@@ -21,53 +14,53 @@ from vmas.simulator.utils import (
   Color
 )
 
-from SenseSphere import SenseSphere
-from BoidDynamics import BoidDynamics
-from Triangle import Triangle
+# from SenseSphere import SenseSphere
+# from BoidDynamics import BoidDynamics
+# from Triangle import Triangle
 
 class MyScenario(BaseScenario):
   #####################################################################
   ###                    make_world function                        ###
   #####################################################################
   def make_world(self, batch_dim: int, device: torch.device, **kwargs):
-    
+
     self.world_size_x = kwargs.pop("world_size_x", 2)
     self.world_size_y = kwargs.pop("world_size_y", 2)
-
     self.plot_grid = True
 
-    ''' INFO FROM KWARGS '''
-    ''' Number of agents '''
-    # self.n_friends = kwargs.pop("friends", 5)
-    # self.n_enemies = kwargs.pop("enemies", 5)
-    # self.n_agents = self.n_friends + self.n_enemies
+    ##############################
+    ###### INFO FROM KWARGS ######
+    ##############################
+
+    ''' Entity spawning '''
+    self.world_spawning_x = kwargs.pop("world_spawning_x", self.world_size_x * 0.9)
+    self.world_spawning_y = kwargs.pop("world_spawning_y", self.world_size_y * 0.5)
+    self.min_distance_between_entities = (kwargs.pop("agent_radius", 0.1) * 2 + 0.05)
+
+    ''' Agent entities '''
     self.n_agents = kwargs.pop("n_agents", 2)
     self.n_teams = kwargs.pop("teams", 2)
 
-    ''' Number of non-agent entities '''
-    # self.n_obstacles = kwargs.pop("n_obstacles", 2)
+    ''' Goal entities '''
     self.n_goals = kwargs.pop("n_goals", 5)
+    self.goal_color = kwargs.pop("goal_colour", Color.YELLOW)
+    self.flat_goal_reward = kwargs.pop("flat_goal_reward", 100)
+    self.goal_range = kwargs.pop("goal_range", 1.0)
+    self.goal_threshold = kwargs.pop("goal_threshold", 2)
+    self.goal_respawn = kwargs.pop("goal_respawn", True)
 
-    ''' cordinates for entities spawning '''
-    self.world_spawning_x = kwargs.pop("world_spawning_x", self.world_size_x * 0.9)
-    self.world_spawning_y = kwargs.pop("world_spawning_y", self.world_size_y * 0.5)
-
-    ''' REWARD INFO '''
-    ''' Penalize collision with another agent or obstacle '''
+    ''' Reward Info '''
     self.agent_collision_penalty = kwargs.pop("agent_collision_penalty", -1)
-    ''' Turning shared reward on '''
+    self.wall_collision_penalty = kwargs.pop("wall_collision_penalty", -1)
     self.shared_rew = kwargs.pop("shared_rew", False)
-
-    ''' Minimum distance between entities at spawning time '''
-    self.min_distance_between_entities = (kwargs.pop("agent_radius", 0.1) * 2 + 0.05)
-    ''' Minimum distance between entities for collision trigger '''
     self.min_collision_distance = (0.005)
 
     ''' Warn if not all kwargs have been consumed '''
     ScenarioUtils.check_kwargs_consumed(kwargs)
 
-
-    '''*********** MAKING THE WORLD ***********'''
+    ####################################
+    ######### MAKING THE WORLD #########
+    ####################################
 
     world = World(
       batch_dim=1,  # Number of environments
@@ -87,12 +80,9 @@ class MyScenario(BaseScenario):
           Color.RED,    # Team 2
           Color.YELLOW # Rewards
     ]
-
     self.goals = []
 
-    # sensors = [SenseSphere(world)]
-
-    ''' Add agents '''
+    ''' Adding agents '''
     self.teams = {}
     for team in range(self.n_teams):
       self.teams[team] = []
@@ -106,11 +96,10 @@ class MyScenario(BaseScenario):
           color=known_colors[team],
           render_action=True,
           sensors=sensors,
-          #shape=Sphere(radius=0.1),     
-          shape=Triangle(),         
+          shape=Triangle(),
           u_range=[1],  # Ranges for actions
           u_multiplier=[1],  # Action multipliers
-          dynamics=BoidDynamics(world=world, team=team) 
+          dynamics=BoidDynamics(world=world, team=team)
         )
 
         agent.pos_rew = torch.zeros(
@@ -123,24 +112,24 @@ class MyScenario(BaseScenario):
         self.teams[team].append(agent)
         world.add_agent(agent)
 
-    ''' Add Goals '''
+    ''' Adding Goals  '''
     for i in range(self.n_goals):
       goal = Landmark(
         name=f"goal_{i}",
-        collide=False,
+        collide=True,
+        movable = False,
         color=known_colors[2],
       )
-      world.add_landmark(goal)
-      # agent.goal = goal
+      goal.range = self.goal_range
+      goal.threshold = self.goal_threshold
       self.goals.append(goal)
-
-    ''' Add later... '''
+      world.add_landmark(goal)
 
     return world
 
-  #####################################################################
-  ###                  reset_world_at function                      ###
-  #####################################################################
+  #####################################################
+  ############## reset_world_at function ##############
+  #####################################################
   def reset_world_at(self, env_index: int = None):
     # Spawn friendlies
     ScenarioUtils.spawn_entities_randomly(
@@ -173,30 +162,123 @@ class MyScenario(BaseScenario):
     for agent in self.world.agents:
       agent.dynamics.reset(0)
 
-  #####################################################################
-  ###                    observation function                       ###
-  #####################################################################
+  ##################################################
+  ############## observation function ##############
+  ##################################################
   def observation(self, agent: Agent):
-    # Measure observations from the agent's sensors
+    ''' Measure observations from the agent's sensors '''
     sensor_observations = [sensor.measure() for sensor in agent.sensors]
-    # Flatten and concatenate sensor data into a single tensor
+    ''' Flatten and concatenate sensor data into a single tensor '''
     obs = {
-        "obs": torch.cat(sensor_observations, dim=-1),
-        "pos": agent.state.pos,
-        "vel": agent.state.vel,
+      "obs": torch.cat(sensor_observations, dim=-1),
+      "pos": agent.state.pos,
+      "vel": agent.state.vel,
     }
     return obs
 
-  #####################################################################
-  ###                       reward function                         ###
-  #####################################################################
+  #############################################
+  ############## reward function ##############
+  #############################################
   def reward(self, agent: Agent):
-    reward = 0.0
+    is_first = agent == self.world.agents[0]
+    is_last = agent == self.world.agents[-1]
 
-    # negative reward for touching boundaries
+    ''' Taken from the discovery.py scenario '''
+    goal_reward = 0
+    if is_first:
+      pass
+      ''' negative reward for time passing - don't think it's relevant for BOIDS '''
+      # self.time_rew = torch.full(
+      #   (self.world.batch_dim,),
+      #   self.time_penalty,
+      #   device=self.world.device,
+      # )
+
+      ''' updating tensor of all agent positions - shape [board_dimensions, num_agents] '''
+      self.agents_pos = torch.stack(
+        [a.state.pos for a in self.world.agents], dim=1
+      )
+      ''' updating tensor of all goal positions '''
+      self.goals_pos = torch.stack([g.state.pos for g in self.goals], dim=1)
+
+      ''' getting tensor with distances between reward positions and agent positions '''
+      self.agents_goals_dists = torch.cdist(self.agents_pos, self.goals_pos)
+
+      self.agents_per_goal = torch.sum(
+        (self.agents_goals_dists < self.goal_range).type(torch.int),
+        dim=1,
+      )
+
+      self.covered_goals = self.agents_per_goal >= self.goal_threshold
+
+      ''' Flat reward for each goal captured by the team '''
+      goal_reward = 0
+      # print(self.covered_goals)
+      # print(self.covered_goals.shape())
+      for goal_covered in self.covered_goals[0]:
+        if goal_covered:
+            goal_reward += self.flat_goal_reward  # Add flat reward for each goal covered
+
+    if is_last:
+      if self.goal_respawn:
+        occupied_positions_agents = [self.agents_pos]
+        for i, goal in enumerate(self.goals):
+          occupied_positions_goals = [
+            o.state.pos.unsqueeze(1)
+            for o in self.goals
+            if o is not goal
+          ]
+          occupied_positions = torch.cat(
+            occupied_positions_agents + occupied_positions_goals,
+            dim=1,
+          )
+          pos = ScenarioUtils.find_random_pos_for_entity(
+            occupied_positions,
+            env_index=None,
+            world=self.world,
+            min_dist_between_entities=self.min_distance_between_entities,
+            x_bounds=(-self.world.x_semidim, self.world.x_semidim),
+            y_bounds=(-self.world.y_semidim, self.world.y_semidim),
+          )
+
+          goal.state.pos[self.covered_goals[:, i]] = pos[
+            self.covered_goals[:, i]
+          ].squeeze(1)
+      else:
+        self.all_time_covered_goals += self.covered_goals
+        for i, goal in enumerate(self.goals):
+          goal.state.pos[self.covered_goals[:, i]] = self.get_outside_pos(
+            None
+          )[self.covered_goals[:, i]]
+
+    ''' Negative reward for touching boundaries (walls) '''
+    coll_pen = 0
     pos_x = agent.state.pos[0][0]
     pos_y = agent.state.pos[0][1]
     if abs(pos_x) == self.world_size_x or abs(pos_y) == self.world_size_y:
-      reward -=1
+      coll_pen = -1  # Apply negative penalty for wall collisions
 
-    return torch.tensor([reward])
+    ''' Combining goal reward and collision penalty '''
+    reward = goal_reward + coll_pen
+
+    ''' Return the total reward (tensor of shape [self.world.batch_dim,]) '''
+    return torch.tensor([reward], device=self.world.device)
+
+  #############################################
+  ############## Extra_render ################
+  #############################################
+  def extra_render(self, env_index: int = 0) -> "List[Geom]":
+    from vmas.simulator import rendering
+
+    geoms: List[Geom] = []
+
+    # Goal covering ranges
+    for goal in self.goals:
+      range_circle = rendering.make_circle(goal.range, filled=False)
+      xform = rendering.Transform()
+      xform.set_translation(*goal.state.pos[env_index])
+      range_circle.add_attr(xform)
+      range_circle.set_color(*self.goal_color.value)
+      geoms.append(range_circle)
+
+    return geoms
